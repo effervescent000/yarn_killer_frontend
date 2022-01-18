62f6be36cd66fd31e76819fc6c1521a1734c4646
effervescent000 <kislio01@gmail.com>
2022-01-18 11:45:34 -0500

    got header implemented

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import catLogoWhite from "../static/assets/cat with yarn white.png";
+
+const Header = (props) => {
+    return (
+        <div id="header">
+            <div id="left-side-header">
+                <div className="nav-bar">
+                    <NavLink to="/yarn/browse">Browse Yarn</NavLink>
+                    <NavLink to="/yarn/add">Add yarn</NavLink>
+                    <NavLink to="/colorways/manage">Colorways</NavLink>
+                </div>
+            </div>
+            <div id="center-header">
+                <div className="logo">
+                    <img src={catLogoWhite} alt="Kitty!" />
+                    <span>Yarn Killer!</span>
+                </div>
+            </div>
+            <div id="right-side-header">
+                <span>Login stuff will go here</span>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = (props) => {
+    return (
+        <div className="content-wrapper">
+            {/* stuff here */}
+            {/* stuff here */}
+        </div>
+    );
+};
+
+export default HomePage;
