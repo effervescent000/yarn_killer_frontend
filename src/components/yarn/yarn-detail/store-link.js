8caa30c452276ac0e8fb63da90c3184da1817b5b
effervescent000 <kislio01@gmail.com>
2022-03-09 14:29:11 -0500

    adding link inputs and styling links

--- src/components/yarn/yarn-detail/store-link.js
+++ src/components/yarn/yarn-detail/store-link.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const StoreLink = (props) => {
-    const { link } = props;
-
+const StoreLink = ({ link }) => {
     return (
-        <div className="link-wrapper link-grid">
-            <div>{link.store.name}</div>
-            <div>{link.current_price}</div>
-            <div>{link.price_updated}</div>
-        </div>
+        // <div className="link-wrapper link-grid">
+        //     <div>{link.store.name}</div>
+        //     <div>{link.current_price}</div>
+        //     <div>{link.price_updated}</div>
+        // </div>
+        <tbody>
+            <tr>
+                <td>{link.store.name}</td>
+                <td>{link.current_price}</td>
+                <td>{link.price_updated}</td>
+            </tr>
+        </tbody>
     );
 };
 
